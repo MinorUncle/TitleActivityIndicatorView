0f1ff947f0a9028ad699d2dbbb88b4af73b5f967
tongguan <tongguan@guangjinminni.local>
2016-03-08 15:29:07 +0800

    Initial Commit

---
+++ ActivityIndicatorView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ActivityIndicatorView
+//
+//  Created by tongguan on 16/3/8.
+//  Copyright © 2016年 tongguan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ActivityIndicatorView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ActivityIndicatorView
+//
+//  Created by tongguan on 16/3/8.
+//  Copyright © 2016年 tongguan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
